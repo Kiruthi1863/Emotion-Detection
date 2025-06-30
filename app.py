32893d26c207d740c012904affddc1d94e0f9cd2
kiruthika <kiruthika1863@gmail.com>
2025-06-30 16:24:07 +0530

    add print in app.py

--- app.py
+++ app.py
@@ -83,6 +83,7 @@ def gen_frames():
 
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
+            print(frame)
 
             yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
